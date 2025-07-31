dfd0f578cfc6fcaa884ec9832e9d1eb976872b14
Asim Sattar <asim.sattar@jeeny.me>
2025-07-31 08:56:00 +0500

    Implement webhook

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements FilamentUser
+//class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -47,8 +48,8 @@ class User extends Authenticatable implements FilamentUser
         ];
     }
 
-    public function canAccessPanel(Panel $panel): bool
-    {
-        return str_ends_with($this->email, '@admin.com') && $this->hasVerifiedEmail();
-    }
+//    public function canAccessPanel(Panel $panel): bool
+//    {
+//        return str_ends_with($this->email, '@admin.com') && $this->hasVerifiedEmail();
+//    }
 }
